76f8c180a9b06ee919eecb3df917ee4f72402fe9
alisouran <alirezasoran@gmail.com>
2020-02-06 21:01:26 +0330

    First Release

--- js/index.js
+++ js/index.js
@@ -2,24 +2,48 @@ function init() {
 
 }
 
-function next() {
-  body.classList.remove('error');
-  var enable = document.querySelector('form fieldset.enable'),
-    nextEnable = enable.nextElementSibling;
-  enable.classList.remove('enable');
-  enable.classList.add('disable');
-  nextEnable.classList.remove('disable');
-  nextEnable.classList.add('enable');
+function next(target) {
+  var input = target.previousElementSibling;
+  if (input.value === '') {
+    body.classList.remove('error');
+    var enable = document.querySelector('form fieldset.enable'),
+      nextEnable = enable.nextElementSibling;
+    enable.classList.remove('enable');
+    enable.classList.add('disable');
+    nextEnable.classList.remove('disable');
+    nextEnable.classList.add('enable');
+  } else {
+    body.classList.remove('error');
+    var enable = document.querySelector('form fieldset.enable'),
+      nextEnable = enable.nextElementSibling;
+    enable.classList.remove('enable');
+    enable.classList.add('disable');
+    nextEnable.classList.remove('disable');
+    nextEnable.classList.add('enable');
+  }
 }
 
-function prev() {
-  body.classList.remove('error');
-  var disable = document.querySelector('form fieldset.enable'),
-    prevEnable = disable.previousElementSibling;
-  disable.classList.remove('enable');
-  disable.classList.add('disable');
-  prevEnable.classList.remove('disable');
-  prevEnable.classList.add('enable');
+function prev(target) {
+  var input;
+  input = target.previousElementSibling;
+  input = input.previousElementSibling;
+  if (input.value === '') {
+    body.classList.remove('error');
+    var disable = document.querySelector('form fieldset.enable'),
+      prevEnable = disable.previousElementSibling;
+    disable.classList.remove('enable');
+    disable.classList.add('disable');
+    prevEnable.classList.remove('disable');
+    prevEnable.classList.add('enable');
+  } else {
+    body.classList.remove('error');
+    var disable = document.querySelector('form fieldset.enable'),
+      prevEnable = disable.previousElementSibling;
+    disable.classList.remove('enable');
+    disable.classList.add('disable');
+    prevEnable.classList.add('enable');
+    prevEnable.classList.remove('disable');
+  }
 }
 
 function keyDown(event) {
@@ -35,7 +59,7 @@ var body = document.querySelector('body'),
 window.onload = init;
 document.body.onmouseup = function (event) {
   var target = event.target || event.toElement;
-  if (target.classList.contains("button-next")) next();
-  else if (target.classList.contains("button-prev")) prev();
+  if (target.classList.contains("button-next")) next(target);
+  else if (target.classList.contains("button-prev")) prev(target);
 };
 document.addEventListener("keydown", keyDown, false);
